b2e3becc2efabbe26bb83742bf1dd2cddbc13005
Izzudin Anuar <izzudinanuar96@gmail.com>
2020-02-01 10:31:53 +1300

    Add packages

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
+import VModal from 'vue-js-modal';
+
+Vue.use(VModal);
 
 Vue.config.productionTip = false
+Vue.component('header', './components/HeaderBar.vue');
 
 new Vue({
   render: h => h(App),
